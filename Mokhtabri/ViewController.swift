0ec37d66c7e1e686edf74e264dc8513d2d98dd05
yasoof <alyasfy12@gmail.com>
2023-11-28 13:31:34 +0300

    Initial Commit

---
+++ Mokhtabri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mokhtabri
+//
+//  Created by Yousif Mohamed Ali Abdulla Salman Alhawaj on 28/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
