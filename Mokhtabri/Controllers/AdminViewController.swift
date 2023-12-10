513d12d0e706eb26541f7071d29e35aa1e128eb0
Yousif <alyasfy12@gmail.com>
2023-12-10 15:05:21 +0300

    changed image design in Admin add screen realigned segmented control in Admin view screen

---
+++ Mokhtabri/Controllers/AdminViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdminViewController.swift
+//  Mokhtabri
+//
+//  Created by Yousif Mohamed Ali Abdulla Salman Alhawaj on 10/12/2023.
+//
+
+import UIKit
+
+class AdminViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
