a5ba265900af93c1b24b409c8b9fec2ec229c8a2
Maram Eskafi <meskafi.me@gmail.com>
2023-12-15 18:14:24 +0300

    Added firebaseAuth to package

--- Mokhtabri/AppDelegate.swift
+++ Mokhtabri/AppDelegate.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
+
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
@@ -15,8 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         AppData.load()
+        FirebaseApp.configure()
         return true
     }
+    
+    
+   
 
     // MARK: UISceneSession Lifecycle
 
