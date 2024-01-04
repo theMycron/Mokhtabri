8e45506d8d37c71893103ffddaea7389eecaea0c
Ali <alawyrose@gmail.com>
2024-01-04 16:24:37 +0300

    date picker was hidden

--- Mokhtabri/Controllers/LabEditTableTableViewController.swift
+++ Mokhtabri/Controllers/LabEditTableTableViewController.swift
@@ -102,8 +102,16 @@ class LabEditTableTableViewController: UITableViewController, UIAdaptivePresenta
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewselecter.isHidden = true
-        DateExpiry.isHidden = true
+        
+        
+        if service is Package{
+            viewselecter.isHidden = false
+            DateExpiry.isHidden = false
+        }
+        if service is Test{
+            viewselecter.isHidden = true
+            DateExpiry.isHidden = true
+        }
         //cellSelect.isHidden = true
         tableView.reloadData()
         // this delegate will control the sheet, and will stop the user from dismissing if changes were made
@@ -209,60 +217,7 @@ class LabEditTableTableViewController: UITableViewController, UIAdaptivePresenta
         isModalInPresentation = hasChanges
         btnSave.isEnabled = hasChanges
     }
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
     
     func uploadImageToFirebase() -> Bool {
         // display warning if no image was selected, can proceed with no image
