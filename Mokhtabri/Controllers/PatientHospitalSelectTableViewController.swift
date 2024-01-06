f3b75ee93698f0e87985f006672e969036a821d4
Fatema <202102290@student.polytechnic.bh>
2024-01-06 14:55:24 +0300

    functional home page for patient

--- Mokhtabri/Controllers/PatientHospitalSelectTableViewController.swift
+++ Mokhtabri/Controllers/PatientHospitalSelectTableViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class PatientHospitalSelectTableViewController: UITableViewController {
     
+    @IBAction func segmentClick(_ sender: UISegmentedControl) {
+        selectedIndex = sender.selectedSegmentIndex
+       // loadData()
+        tableView.reloadData()    }
     
+    func filter(){
+
+    }
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     // declare variables
     var selectedHospital: MedicalFacility?
-    var listOfTests: [MedicalService] = []
-    
+    var listOfTests: [MedicalService] = AppData.services
+    var selectedIndex = 0
+    var tests : [Test] = []
+    var packages: [Package] = []
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let destination = segue.destination as? PatientBookTableViewController, let selectedRow = tableView.indexPathForSelectedRow{
             destination.sampleTest = listOfTests[selectedRow.row]
@@ -44,67 +55,130 @@ class PatientHospitalSelectTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        if section == 0 {
-            return 2
-        }else {
-            return listOfTests.count
+        if selectedIndex == 0{
+            if section == 0 {
+                return 2
+            } else {
+                return tests.count
+            }
+        } else {
+            if section == 0 {
+                return 2
+            }else{
+                return packages.count
+            }
         }
+        
+        
     }
     
     func loadData() {
-        let listOfAllTests = AppData.services
+        
+        //let listOfAllTests = AppData.services
         guard let hospital = selectedHospital else {
             return
         }
         
-        for test in listOfAllTests {
+     /*   for test in listOfTests {
             if test.forMedicalFacility.name == hospital.name {
             listOfTests.append(test)
-                
+            }
+        }*/
+        
+        for service in listOfTests {
+            if service is Package && service.forMedicalFacility == hospital{
+                packages.append(service as! Package)
+            } else if service is Test && service.forMedicalFacility == hospital {
+                tests.append(service as! Test)
             }
         }
+        
+        
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 1{
-            let cell = tableView.dequeueReusableCell(withIdentifier: "PatientBooking", for: indexPath) as! PatientBookingTableViewCell
+        if selectedIndex == 0{
+            if indexPath.section == 1{
+                let cell = tableView.dequeueReusableCell(withIdentifier: "PatientBooking", for: indexPath) as! PatientBookingTableViewCell
 
-            cell.TestName.text = listOfTests[indexPath.row].name
-            cell.price.text = "\(listOfTests[indexPath.row].price) BHD"
-            cell.hospitalName.text = "\(listOfTests[indexPath.row].forMedicalFacility.name)"
-
-            
-    
-            return cell
-        }else {
-            if indexPath.row == 1 {
-                let cell = tableView.dequeueReusableCell(withIdentifier: "infocell", for: indexPath) as! PatientViewInfoTableViewCell
-                
-                if selectedHospital?.alwaysOpen == true {
-                    if let hospitalName = selectedHospital?.name, let hospitalCity = selectedHospital?.city {
-                        cell.info.text = "\(hospitalName) - \(hospitalCity) - Open 24 Hours"
-                    } else {
-                        // Handle the case where either name or city is nil
-                        cell.info.text = "Hospital Information Not Available"
-                    }
-                } else {
-                    if let hospitalName = selectedHospital?.name, let hospitalCity = selectedHospital?.city, let openingTime = selectedHospital?.openingTime.hour, let closingTime = selectedHospital?.closingTime.hour {
-                        cell.info.text = "\(hospitalName) - \(hospitalCity)  Timing: \(openingTime):00 - \(closingTime):00"
-                    } else {
-                        // Handle the case where any of the required information is nil
-                        cell.info.text = "Hospital Information Not Available"
-                    }
-
-                }
+                cell.TestName.text = tests[indexPath.row].name
+                cell.price.text = "\(tests[indexPath.row].price) BHD"
+                cell.hospitalName.text = "\(tests[indexPath.row].forMedicalFacility.name)"
 
                 
-                return cell
-            } else {
-                let cell = tableView.dequeueReusableCell(withIdentifier: "imagecell", for: indexPath) as! imgTableViewCell
-                cell.img.image = UIImage(named: "AlHilal")
-                cell.img.frame = CGRect(x: 76, y: 0, width: 201, height: 158)
+        
                 return cell
             }
+            else {
+                if indexPath.row == 1 {
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "infocell", for: indexPath) as! PatientViewInfoTableViewCell
+                    
+                    if selectedHospital?.alwaysOpen == true {
+                        if let hospitalName = selectedHospital?.name, let hospitalCity = selectedHospital?.city {
+                            cell.info.text = "\(hospitalName) - \(hospitalCity) - Open 24 Hours"
+                        } else {
+                            // Handle the case where either name or city is nil
+                            cell.info.text = "Hospital Information Not Available"
+                        }
+                    } else {
+                        if let hospitalName = selectedHospital?.name, let hospitalCity = selectedHospital?.city, let openingTime = selectedHospital?.openingTime.hour, let closingTime = selectedHospital?.closingTime.hour {
+                            cell.info.text = "\(hospitalName) - \(hospitalCity)  Timing: \(openingTime):00 - \(closingTime):00"
+                        } else {
+                            // if there's missing data
+                            cell.info.text = "Hospital Information Not Available"
+                        }
+                    }
+                    
+                    return cell
+                } else {
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "imagecell", for: indexPath) as! imgTableViewCell
+                    cell.img.image = UIImage(named: "AlHilal")
+                    cell.img.frame = CGRect(x: 76, y: 0, width: 201, height: 158)
+                    return cell
+                }
+            }
+
+        } else {
+            if indexPath.section == 1{
+                let cell = tableView.dequeueReusableCell(withIdentifier: "PatientBooking", for: indexPath) as! PatientBookingTableViewCell
+
+                cell.TestName.text = packages[indexPath.row].name
+                cell.price.text = "\(packages[indexPath.row].price) BHD"
+                cell.hospitalName.text = "\(packages[indexPath.row].forMedicalFacility.name)"
+
+                
+        
+                return cell
+            }
+            else {
+                if indexPath.row == 1 {
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "infocell", for: indexPath) as! PatientViewInfoTableViewCell
+                    
+                    if selectedHospital?.alwaysOpen == true {
+                        if let hospitalName = selectedHospital?.name, let hospitalCity = selectedHospital?.city {
+                            cell.info.text = "\(hospitalName) - \(hospitalCity) - Open 24 Hours"
+                        } else {
+                            // Handle the case where either name or city is nil
+                            cell.info.text = "Hospital Information Not Available"
+                        }
+                    } else {
+                        if let hospitalName = selectedHospital?.name, let hospitalCity = selectedHospital?.city, let openingTime = selectedHospital?.openingTime.hour, let closingTime = selectedHospital?.closingTime.hour {
+                            cell.info.text = "\(hospitalName) - \(hospitalCity)  Timing: \(openingTime):00 - \(closingTime):00"
+                        } else {
+                            // if there's missing data
+                            cell.info.text = "Hospital Information Not Available"
+                        }
+                    }
+                    
+                    return cell
+                } else {
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "imagecell", for: indexPath) as! imgTableViewCell
+                    cell.img.image = UIImage(named: "AlHilal")
+                    cell.img.frame = CGRect(x: 76, y: 0, width: 201, height: 158)
+                    return cell
+                }
+            }
+
         }
     }
 
