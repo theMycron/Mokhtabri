5dcbf53be9e96091a7bb048fe768e4bf848b155e
ali abdulrasool <alawyrose@gmail.com>
2024-01-07 15:38:44 +0300

    more fixed to select tests

--- Mokhtabri/Controllers/LabSelectTestsTableViewController.swift
+++ Mokhtabri/Controllers/LabSelectTestsTableViewController.swift
@@ -8,34 +8,66 @@
 import UIKit
 import SwiftUI
 
-class LabSelectTestsTableViewController: UITableViewController {
-    required init?(coder: NSCoder) {
+class LabSelectTestsTableViewController: UITableViewController, UIAdaptivePresentationControllerDelegate {
+    var selectedRows = Set<Int>()
+    var package: Package?
+    var selectedTests: [Test] = []
+    let facility: MedicalFacility = AppData.loggedInUser as! MedicalFacility
+    init?(coder: NSCoder, package: Package?) {
+        self.package = package
+
+        
         displayedTests = []
-        let tests: [Test] = AppData.listOfTests.compactMap {$0 as? Test}
-        for test in tests {
-            if (test.forMedicalFacility == facility) {
-                displayedTests.append(test)
+        super.init(coder: coder)
+        displayedTests = AppData.tests.filter{$0.forMedicalFacility == self.facility}
+        
+        if let package = package {
+            selectedTests = package.tests
+            guard selectedTests.count > 0 else {return}
+            for i in 0...selectedTests.count-1{
+                if displayedTests.contains(selectedTests[i]) {
+                    selectedRows.insert(displayedTests.firstIndex(of: selectedTests[i])!)
+                }
             }
         }
+        
+    }
+    required init?(coder: NSCoder) {
+        displayedTests = []
+        
+        displayedTests = []
         super.init(coder: coder)
+        displayedTests = AppData.tests.filter{$0.forMedicalFacility == self.facility}
     }
     
-    let facility = AppData.alhilal
+
 
     
     
     var displayedTests: [Test]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        presentationController?.delegate = self
+        isModalInPresentation = true
             tableView.allowsMultipleSelection = true
             tableView.allowsMultipleSelectionDuringEditing = true
+        for row in selectedRows {
+               tableView.selectRow(at: IndexPath(row: row, section: 0), animated: false, scrollPosition: .none)
+           }
+        
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
+    func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) {
+        self.performSegue(withIdentifier: "unwindFromSelect", sender: self)
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -48,15 +80,38 @@ class LabSelectTestsTableViewController: UITableViewController {
         return displayedTests.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->LabSelectTestTableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TestCell", for: indexPath) as! LabSelectTestTableViewCell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "TestCell", for: indexPath) as! LabSelectTestTableViewCell
 
-        let test: Test = displayedTests[indexPath.row]
-        cell.update(with: test)
+       let test: Test = displayedTests[indexPath.row]
+       cell.update(with: test)
 
-        return cell
+       if selectedRows.contains(indexPath.row) {
+           // Update the cell's appearance to indicate it's selected
+       
+           cell.isSelected = true
+       } else {
+           // Update the cell's appearance to indicate it's not selected
+         
+           cell.isSelected = false
+       }
+
+       return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard package != nil else {return}
+        selectedRows.insert(indexPath.row)
+        selectedTests.append(displayedTests[indexPath.row])
+        
     }
     
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        guard package != nil else {return}
+        selectedRows.remove(indexPath.row)
+        selectedTests.remove(at: selectedTests.firstIndex(of: displayedTests[indexPath.row])!)
+    }
+
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
